d342af03020edd104d5c68f47079865568cf1033
Caio Oliveira <caio.vsoliveira23@gmail.com>
2020-08-31 14:45:10 -0300

    feat: add undo operations

--- tests/unit/calculatorState.test.ts
+++ tests/unit/calculatorState.test.ts
@@ -18,4 +18,29 @@ describe('calculator state', () => {
 		expect(reset).toBe(0);
 	});
 
+	it('undo inexistent operation', async () => {
+		const calculator = new Calculator();
+
+		const five = calculator.reset(5).undo().resolve();
+
+		expect(five).toBe(5);
+	});
+
+	it('undo operations', async () => {
+		const calculator = new Calculator();
+
+		const thirteen = calculator.reset(10).add(3).subtract(1).multiply(5).divide(2).add(13)
+			.undo().undo().undo().undo().resolve();
+
+		expect(thirteen).toBe(13);
+	});
+
+	it('resolve expression', async () => {
+		const calculator = new Calculator();
+
+		const twenty = calculator.reset(10).add(4).multiply(3).divide(4).add(10).subtract(6).divide(2).resolve();
+
+		expect(twenty).toBe(20);
+	});
+
 });
